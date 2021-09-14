e4f2df33e793187100d68217f601c53f9c71cbf7
vcnc-hex <hex@vcnc.co.kr>
2021-09-14 21:08:19 +0900

    testing filter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const input: string = core.getInput('input')
+    const filter: string = core.getInput('filter')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const filter_fn = new Function(filter)
+    core.setOutput('json', JSON.stringify(JSON.parse(input).filter(filter_fn)))
   } catch (error) {
     core.setFailed(error.message)
   }
