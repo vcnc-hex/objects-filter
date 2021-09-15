477fb13189e71c2010ed92cc40425975b9f6c640
vcnc-hex <hex@vcnc.co.kr>
2021-09-15 12:49:32 +0900

    revamp to action.yml

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const input: string = core.getInput('input')
+    const json: string = core.getInput('json')
     const filter: string = core.getInput('filter')
-
-    const filter_fn = new Function('label', filter)
-    const output = JSON.stringify(JSON.parse(input).filter(filter_fn))
+    const filter_fn = new Function('input', filter)
+    const output = JSON.stringify({
+      include: JSON.parse(json).filter(filter_fn)
+    })
     core.setOutput('json', output)
   } catch (error) {
     core.setFailed(error.message)
